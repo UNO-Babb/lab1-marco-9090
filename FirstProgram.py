f93e1edc02ad6abbfae374a56df175e14a5c5064
marco-9090 <marcanthonywilliam@unomaha.edu>
2024-09-01 06:03:02 +0000

    Finished Lab 1

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,20 +1,28 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Marc Anthony Williams
+#Date: 08/31/2024
+#Assignment: Lab 1 
+#Purpose: To obtain users name and birth year. 
 
 def main():
   print("First Program")
-  #Say hello
+  #Say hello 
+  print("hello")
   
   #Ask for the user's name
+  name = input("Enter name here: ")
+  
 
   #Use the user's name in the program.
-
+  print("HOw is your day going", name)
   #Ask the user for their age.
-
+  age = input("Enter your age:")
+  age = int(age) 
   #Tell the user what year they were born in.
+  born = 2022 - age
+
   #Assume that they have not had their birthday yet this year.
+  print("You were born in", born)
 
 
 #Call the main function if this is the file being run.
--- MadLib.py
+++ MadLib.py
@@ -1,14 +1,18 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Marc Anthony Williams
+#Date: 08/31/2024
+#Assignment: Lab 1 
+#Purpose: Ask user for words to create a story
 
 def main():
   print("Madlib")
   #Ask user for words
+  noun1 = input("Enter a noun: ")
+  noun2 = input("Enter a noun: ")
 
   #Print the story with the user supplied words.
-
+  print("I saw a" + noun1 + "when riding my bike" + noun2 + "oh what a scene.")
+  print("I saw a" , noun1 , "when riding my bike" , noun2 , "oh what a scene.")
 
 
 #Call the main function if this is the file being run.
